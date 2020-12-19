cc3d2f7589ea52eba2a324bb76407e93b2d1460a
Alan Grier <griera@oregonstate.edu>
2020-12-18 20:03:30 -0800

    add basic driving code

--- locomotive_receiver.ino
+++ locomotive_receiver.ino
@@ -15,26 +15,27 @@ RH_RF69 driver(RFM69_CS, RFM69_INT);
 RHReliableDatagram manager(driver, SERVER_ADDRESS);
 uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
 
-TwoPinMotor TwoPinMotor(-1, -1, -1, -1);
+// Pin assignments
+#define PIN_TRACK -1 // IO Pin for reading track voltage
+#define PIN_LIGHT_FRONT -1
+#define PIN_LIGHT_REAR -1
+#define PIN_LIGHT_CAB -1
+#define PIN_MOTOR1 -1
+#define PIN_MOTOR2 -1
+#define PIN_FUNC1 -1
+#define PIN_FUNC2 -1
 
-// // create list of registers for MAX_MAIN_REGISTER Main Track Packets
-// volatile RegisterList mainRegs(1);
-// uint8_t *command;
+// Various others
+#define TRACK_THRESHOLD -1 // When to disable train due to undervoltage battery
 
-// // DCC output pins
-// #define DCC_1 9
-// #define DCC_2 10
-
-// // Timer duration and interrupt values for 0/1 bits
-// #define DCC_ZERO_BIT_TOTAL_DURATION 199
-// #define DCC_ZERO_BIT_PULSE_DURATION 99
-// #define DCC_ONE_BIT_TOTAL_DURATION 115
-// #define DCC_ONE_BIT_PULSE_DURATION 57
+Lighting front_light = Lighting(PIN_LIGHT_FRONT);
+Lighting rear_light = Lighting(PIN_LIGHT_REAR);
+Lighting cab_light = Lighting(PIN_LIGHT_CAB);
+TwoPinMotor locomotive = TwoPinMotor(PIN_MOTOR1, PIN_MOTOR2, &front_light, &rear_light);
+long timer_disable = millis();
 
 void setup()
 {
-    // SerialCommand::init(&mainRegs); // create structure to read and parse commands from serial line
-
     Serial.begin(115200); // configure serial interface
     Serial.flush();
 
@@ -47,46 +48,17 @@ void setup()
     driver.setEncryptionKey(key);
     manager.setTimeout(50);
 
-    // // Set DCC pins out output
-    // pinMode(DCC_1, OUTPUT);
-    // pinMode(DCC_2, OUTPUT);
-
-    // // 32u4 Timer 4, 10 bit
-    // // Enable Timer 4B output
-    // bitSet(DDRB, 6);
-
-    // // Set waveform mode to up/reset
-    // bitClear(TCCR4D, WGM40);
-    // bitClear(TCCR4D, WGM41);
-
-    // // Set prescale to /8
-    // bitClear(TCCR4B, CS40);
-    // bitClear(TCCR4B, CS41);
-    // bitSet(TCCR4B, CS42);
-    // bitClear(TCCR4B, CS43);
-
-    // // Set comparator to toggle/inverted
-    // bitSet(TCCR4A, PWM4B);
-    // bitSet(TCCR4A, COM4B0);
-    // bitClear(TCCR4A, COM4B1);
-
-    // // Set one bits as default write
-    // OCR4C = DCC_ONE_BIT_TOTAL_DURATION;
-    // OCR4B = DCC_ONE_BIT_PULSE_DURATION;
-
-    // // load idle packet into register 1
-    // mainRegs.loadPacket(1, RegisterList::idlePacket, 2, 0);
-
-    // // Enable interrupt vector
-    // bitSet(TIMSK4, OCIE4B);
-
-    pinMode(13, OUTPUT);
-    digitalWrite(13, HIGH);
+    pinMode(PIN_TRACK, INPUT);
+    // pinMode(13, OUTPUT);
+    // digitalWrite(13, HIGH);
+    cab_light.On();
 }
 
 void loop()
 {
-    // command = 0;
+    if (analogRead(PIN_TRACK) < TRACK_THRESHOLD)
+        shutdown();
+
     if (manager.available())
     {
         // Serial.println("manager");
@@ -99,56 +71,27 @@ void loop()
             //Serial.print(": ");
             // Serial.println(len);
             // Serial.println((char*)buf);
-            // command = buf;
-            // SerialCommand::process(command); // check for, and process, and new serial commands
+
+            if (buf[0] == "drive at a speed")
+            {
+                int spd = buf[1];
+                //Serial.print(spd);
+                //Serial.println(dir);
+                Serial.println(spd * dir);
+                locomotive.setSpeed(spd * dir);
+                timer_disable = millis();
+            }
         }
     }
+
+    if (millis() - timer_disable > 500)
+        locomotive.disable();
 }
 
-// #define DCC_SIGNAL                                                                                                                                                                          \
-//     if (mainRegs.currentBit == mainRegs.currentReg->activePacket->nBits)                                                                                                                    \
-//     {                            /* IF no more bits in this DCC Packet */                                                                                                                   \
-//         mainRegs.currentBit = 0; /* reset current bit pointer and determine which Register and Packet to process next--- */                                                                 \
-//                                                                                                                                                                                             \
-//         if (mainRegs.nRepeat > 0 && mainRegs.currentReg == mainRegs.reg)                                                                                                                    \
-//         {                       /* IF current Register is first Register AND should be repeated */                                                                                          \
-//             mainRegs.nRepeat--; /* decrement repeat count; result is this same Packet will be repeated */                                                                                   \
-//         }                                                                                                                                                                                   \
-//         else if (mainRegs.nextReg != NULL)                                                                                                                                                  \
-//         {                                                            /* ELSE IF another Register has been updated */                                                                        \
-//             mainRegs.currentReg = mainRegs.nextReg;                  /* update currentReg to nextReg */                                                                                     \
-//             mainRegs.nextReg = NULL;                                 /* reset nextReg to NULL */                                                                                            \
-//             mainRegs.tempPacket = mainRegs.currentReg->activePacket; /* flip active and update Packets */                                                                                   \
-//                                                                                                                                                                                             \
-//             mainRegs.currentReg->activePacket = mainRegs.currentReg->updatePacket;                                                                                                          \
-//             mainRegs.currentReg->updatePacket = mainRegs.tempPacket;                                                                                                                        \
-//         }                                                                                                                                                                                   \
-//         else                                                                                                                                                                                \
-//         {                                                     /* ELSE simply move to next Register */                                                                                       \
-//             if (mainRegs.currentReg == mainRegs.maxLoadedReg) /* BUT IF this is last Register loaded */                                                                                     \
-//                 mainRegs.currentReg = mainRegs.reg;           /* first reset currentReg to base Register, THEN */                                                                           \
-//             mainRegs.currentReg++;                            /* increment current Register (note this logic causes Register[0] to be skipped when simply cycling through all Registers) */ \
-//         }                                                                                                                                                                                   \
-//     } /* END-IF: currentReg, activePacket, and currentBit should now be properly set to point to next DCC bit */                                                                            \
-//                                                                                                                                                                                             \
-//     if (mainRegs.currentReg->activePacket->buf[mainRegs.currentBit / 8] & mainRegs.bitMask[mainRegs.currentBit % 8])                                                                        \
-//     {                                       /* IF bit is a ONE */                                                                                                                           \
-//         OCR4C = DCC_ONE_BIT_TOTAL_DURATION; /* set OCRA for timer N to full cycle duration of DCC ONE bit */                                                                                \
-//         OCR4B = DCC_ONE_BIT_PULSE_DURATION; /* set OCRB for timer N to half cycle duration of DCC ONE but */                                                                                \
-//     }                                                                                                                                                                                       \
-//     else                                                                                                                                                                                    \
-//     {                                        /* ELSE it is a ZERO */                                                                                                                        \
-//         OCR4C = DCC_ZERO_BIT_TOTAL_DURATION; /* set OCRA for timer N to full cycle duration of DCC ZERO bit */                                                                              \
-//         OCR4B = DCC_ZERO_BIT_PULSE_DURATION; /* set OCRB for timer N to half cycle duration of DCC ZERO bit */                                                                              \
-//     }                                        /* END-ELSE */                                                                                                                                 \
-//                                                                                                                                                                                             \
-//     mainRegs.currentBit++; /* point to next bit in current Packet */
-
-// ///////////////////////////////////////////////////////////////////////////////
-
-// // NOW USE THE ABOVE MACRO TO CREATE THE CODE FOR EACH INTERRUPT
-
-// ISR(TIMER4_COMPB_vect)
-// { // set interrupt service for OCR1B of TIMER-1 which flips direction bit of Motor Shield Channel A controlling Main Track
-//     DCC_SIGNAL
-// }
+void shutdown()
+{
+    locomotive.disable(true);
+    cab_light.Off();
+    noInterrupts();
+    while(1) {}
+}
